d3fbb5aa26fe249742c7b4a0bfc8fe971e89c663
Mariia Donskaia <54943996+dma117@users.noreply.github.com>
2024-05-06 23:33:40 +0500

    refactor: KAN-44 Prepare Index HTML for Development (#6)
    * rafactor: initialize root div outside index.html file
    
    * refactor: change title in index.html to be more appropriate

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(getRootDiv()).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
+
+function getRootDiv(): HTMLDivElement {
+  const rootDiv = document.createElement('div');
+  rootDiv.id = 'root';
+  document.body.append(rootDiv);
+
+  return rootDiv;
+}
