27aa57633180c625575780e57a8008772a229795
Ilana Lipkina <ilanalipkina@gmail.com>
2024-05-12 10:46:11 +0400

    refactor: move scopes variable to environment file

--- src/utils/build-client.ts
+++ src/utils/build-client.ts
@@ -4,43 +4,10 @@ import {
   type HttpMiddlewareOptions,
 } from '@commercetools/sdk-client-v2';
 
-const scopes = [
-  'view_payments:online-store-dds',
-  'view_standalone_prices:online-store-dds',
-  'view_connectors:online-store-dds',
-  'view_project_settings:online-store-dds',
-  'view_discount_codes:online-store-dds',
-  'view_products:online-store-dds',
-  'view_customers:online-store-dds',
-  'view_connectors_deployments:online-store-dds',
-  'view_tax_categories:online-store-dds',
-  'view_business_units:online-store-dds',
-  'view_attribute_groups:online-store-dds',
-  'view_cart_discounts:online-store-dds',
-  'view_customer_groups:online-store-dds',
-  'view_audit_log:online-store-dds',
-  'view_order_edits:online-store-dds',
-  'view_messages:online-store-dds',
-  'view_shopping_lists:online-store-dds',
-  'view_staged_quotes:online-store-dds',
-  'view_orders:online-store-dds',
-  'view_import_containers:online-store-dds',
-  'view_categories:online-store-dds',
-  'view_quotes:online-store-dds',
-  'view_shipping_methods:online-store-dds',
-  'view_sessions:online-store-dds',
-  'view_states:online-store-dds',
-  'view_associate_roles:online-store-dds',
-  'view_types:online-store-dds',
-  'view_quote_requests:online-store-dds',
-  'view_product_selections:online-store-dds',
-  'view_key_value_documents:online-store-dds',
-  'view_published_products:online-store-dds',
-  'view_stores:online-store-dds',
-];
+const scopes = import.meta.env.VITE_SCOPES.split(' ');
 
 const authMiddlewareOptions: AuthMiddlewareOptions = {
-  host: import.meta.env.VITE_AUTH_HOST,
+  host: import.meta.env.VITE_AUTH_URL,
   projectKey: import.meta.env.VITE_PROJECT_KEY,
   credentials: {
     clientId: import.meta.env.VITE_CLIENT_ID,
@@ -51,7 +18,7 @@ const authMiddlewareOptions: AuthMiddlewareOptions = {
 };
 
 const httpMiddlewareOptions: HttpMiddlewareOptions = {
-  host: import.meta.env.VITE_API_HOST,
+  host: import.meta.env.VITE_API_URL,
   fetch,
 };
 
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,8 +4,9 @@ interface ImportMetaEnv {
   readonly VITE_PROJECT_KEY: string
   readonly VITE_CLIENT_ID: string
   readonly VITE_CLIENT_SECRET: string
-  readonly VITE_AUTH_HOST: string
-  readonly VITE_API_HOST: string
+  readonly VITE_AUTH_URL: string
+  readonly VITE_API_URL: string
+  readonly VITE_SCOPES: string
 }
 
 interface ImportMeta {
