99cbc026b2d585db6067e7846f2682298e32f7cf
Ilana Lipkina <ilanalipkina@gmail.com>
2024-05-16 16:26:37 +0400

    feat: add login and register methods

--- src/utils/build-client.ts
+++ src/utils/build-client.ts
@@ -1,29 +1,73 @@
 import {
+  Client,
   ClientBuilder,
-  type AuthMiddlewareOptions,
+  PasswordAuthMiddlewareOptions,
+  TokenCache,
   type HttpMiddlewareOptions,
 } from '@commercetools/sdk-client-v2';
 
 const scopes = import.meta.env.VITE_SCOPES.split(' ');
-
-const authMiddlewareOptions: AuthMiddlewareOptions = {
-  host: import.meta.env.VITE_AUTH_URL,
-  projectKey: import.meta.env.VITE_PROJECT_KEY,
-  credentials: {
-    clientId: import.meta.env.VITE_CLIENT_ID,
-    clientSecret: import.meta.env.VITE_CLIENT_SECRET,
-  },
-  scopes,
-  fetch,
-};
-
 const httpMiddlewareOptions: HttpMiddlewareOptions = {
   host: import.meta.env.VITE_API_URL,
   fetch,
 };
 
-export const ctpClient = new ClientBuilder()
-  .withClientCredentialsFlow(authMiddlewareOptions)
-  .withHttpMiddleware(httpMiddlewareOptions)
-  .withLoggerMiddleware()
-  .build();
+type AuthMiddlewareOptions = {
+  host: string;
+  projectKey: string;
+  credentials: {
+    clientId: string;
+    clientSecret: string;
+    anonymousId?: string;
+    user?: {
+      username: string;
+      password: string;
+      activeCartSignInMode: string;
+    };
+  };
+  scopes?: Array<string>;
+  oauthUri?: string;
+  fetch?: typeof fetch;
+  tokenCache?: TokenCache;
+};
+function createAuthOptions(username?: string, password?: string): AuthMiddlewareOptions {
+  const options: AuthMiddlewareOptions = {
+    host: import.meta.env.VITE_AUTH_URL,
+    projectKey: import.meta.env.VITE_PROJECT_KEY,
+    credentials: {
+      clientId: import.meta.env.VITE_CLIENT_ID,
+      clientSecret: import.meta.env.VITE_CLIENT_SECRET,
+    },
+    scopes,
+    fetch,
+  };
+  if (username && password) {
+    options.credentials.user = {
+      username,
+      password,
+      activeCartSignInMode: 'MergeWithExistingCustomerCart',
+    };
+  }
+  return options;
+}
+
+function createSession(options: AuthMiddlewareOptions): Client {
+  const clientBuilder = new ClientBuilder()
+    .withProjectKey(options.projectKey)
+    .withHttpMiddleware(httpMiddlewareOptions);
+  if (options.credentials.user) {
+    return clientBuilder.withPasswordFlow(options as PasswordAuthMiddlewareOptions).build();
+  } else {
+    return clientBuilder.withAnonymousSessionFlow(options).build();
+  }
+}
+
+export function createAnonymousSession(): Client {
+  const options = createAuthOptions();
+  return createSession(options);
+}
+
+export function createAuthorizedSession(username: string, password: string): Client {
+  const options = createAuthOptions(username, password);
+  return createSession(options);
+}
--- src/utils/commercetools-api.ts
+++ src/utils/commercetools-api.ts
@@ -1,16 +1,67 @@
-import { ctpClient } from './build-client';
-import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
+import { Client } from '@commercetools/sdk-client-v2';
+import { createAnonymousSession, createAuthorizedSession } from './build-client';
+import {
+  ByProjectKeyRequestBuilder,
+  createApiBuilderFromCtpClient,
+  MyCustomerDraft,
+} from '@commercetools/platform-sdk';
 
-const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
-  projectKey: import.meta.env.VITE_PROJECT_KEY,
-});
+function createApiRoot(session: Client): ByProjectKeyRequestBuilder {
+  const apiRoot = createApiBuilderFromCtpClient(session).withProjectKey({
+    projectKey: import.meta.env.VITE_PROJECT_KEY,
+  });
+  return apiRoot;
+}
 
-// TODO: Consider if it's necessary to delete the code below.
-// Example call to return Project information
-// This code has the same effect as sending a GET request to the commercetools Composable Commerce API without any endpoints.
-export const getProject = () => {
-  return apiRoot.get().execute();
-};
+export function createAnonymousApiBuilder(): ByProjectKeyRequestBuilder {
+  return createApiRoot(createAnonymousSession());
+}
 
-// Retrieve Project information and output the result to the log
-getProject().then(console.log).catch(console.error);
+function createAuthorizedApiBuilder(
+  username: string,
+  password: string,
+): ByProjectKeyRequestBuilder {
+  return createApiRoot(createAuthorizedSession(username, password));
+}
+
+export function login(
+  apiRoot: ByProjectKeyRequestBuilder,
+  email: string,
+  password: string,
+): Promise<ByProjectKeyRequestBuilder> {
+  return apiRoot
+    .me()
+    .login()
+    .post({
+      body: {
+        email,
+        password,
+        activeCartSignInMode: 'MergeWithExistingCustomerCart',
+      },
+    })
+    .execute()
+    .catch((error) => {
+      console.error('Error during API request:', error);
+    })
+    .then(() => {
+      return createAuthorizedApiBuilder(email, password);
+    });
+}
+
+export function register(apiRoot: ByProjectKeyRequestBuilder, customerDraft: MyCustomerDraft) {
+  return apiRoot
+    .me()
+    .signup()
+    .post({ body: customerDraft })
+    .execute()
+    .then((response) => {
+      if (response.statusCode === 201) {
+        return createAuthorizedApiBuilder(customerDraft.email, customerDraft.password);
+      } else {
+        console.error('Error while registering user:', response);
+      }
+    })
+    .catch((error) => {
+      console.error('Error while registering user:', error);
+    });
+}
