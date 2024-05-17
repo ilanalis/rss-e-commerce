1a6d71a425a89076a8aea16da04b7b3deb090608
maria <ysters14@gmail.com>
2024-05-18 01:59:16 +0500

    chore: configure stylelint

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import stylelint from 'vite-plugin-stylelint';
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
@@ -8,12 +7,5 @@ export default defineConfig({
   plugins: [
     react(),
     nodePolyfills(),
-    stylelint({
-      fix: true,
-      cache: false,
-      config: {
-        extends: ["stylelint-config-css-modules"],
-      },
-    }),
   ],
 });
