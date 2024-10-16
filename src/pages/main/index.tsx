e42b2b344d8d6c33bac1b9a6853625767fcec671
ilana <ilanalipkina@gmail.com>
2024-10-16 17:06:39 +0400

    feat: add description

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,8 +1,8 @@
-import { Link } from 'react-router-dom';
 import styles from './main.module.css';
 import cn from 'classnames';
-import { Routes } from '@/utils/const';
 import { discountCodes } from './config';
+import descrImg1 from '@assets/descr-1.png';
+import descrImg2 from '@assets/descr-2.png';
 
 function Main() {
   return (
@@ -21,37 +21,52 @@ function Main() {
           </div>
         ))}
       </div>
-      <ul>
-        <li className={styles.navItem}>
-          <Link className={styles.link} to={Routes.CATALOG}>
-            <i className={cn(styles.icon, styles.coursesIcon)}></i>
-            Catalog
-          </Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link className={styles.link} to={Routes.ABOUT}>
-            About us
-          </Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link className={styles.link} to={Routes.CART}>
-            <i className={cn(styles.icon, styles.basketIcon)}></i>
-            Basket
-          </Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link className={styles.link} to={Routes.LOGIN}>
-            <i className={cn(styles.icon, styles.loginIcon)}></i>
-            Login
-          </Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link className={styles.link} to={Routes.SIGNUP}>
-            <i className={cn(styles.icon, styles.signupIcon)}></i>
-            Sign Up
-          </Link>
-        </li>
-      </ul>
+      <div className={styles.about}>
+        <h1 className={styles.header}>Welcome to our online course store!</h1>
+        <div className={styles.descriptionBlock}>
+          <p className={styles.text}>
+            Our online store offers a wide selection of courses on various topics, from programming
+            and design to marketing and personal development. We bring together{' '}
+            <b className={styles.highlightedTextBlue}>top instructors</b> and
+            <b className={styles.highlightedTextBlue}> high-quality educational materials</b> to
+            provide you with affordable and convenient learning anytime, anywhere. Each course is
+            designed with <b className={styles.highlightedTextBlue}>modern trends</b> and
+            <b className={styles.highlightedTextBlue}>practical skills</b> in mind, helping you
+            succeed in your chosen field.
+          </p>
+          <div className={styles.imgWrapper}>
+            <img src={descrImg1} alt="" />
+          </div>
+        </div>
+        <div className={styles.descriptionBlock}>
+          <div className={cn(styles.imgWrapper, styles.imgWrapper2)}>
+            <img src={descrImg2} alt="" />
+          </div>
+          <p className={styles.text}>
+            <h2 className={styles.subtitle}>
+              Why choose <b className={styles.highlightedTextBlue}>us</b>:
+            </h2>
+            <ul className={styles.criteriaList}>
+              <li>
+                <b className={styles.highlightedTextBlue}>Wide variety of courses:</b> A diverse
+                range of subjects and learning paths for all skill levels.
+              </li>
+              <li>
+                <b className={styles.highlightedTextBlue}>Flexible format:</b> Learn online at your
+                own pace with 24/7 access to course materials.
+              </li>
+              <li>
+                <b className={styles.highlightedTextBlue}>Practical focus:</b> Emphasis on
+                real-world tasks and case studies for better knowledge retention.
+              </li>
+              <li>
+                <b className={styles.highlightedTextBlue}>Constantly updated content:</b> We
+                regularly add new courses to keep you up-to-date with the latest trends.
+              </li>
+            </ul>
+          </p>
+        </div>
+      </div>{' '}
     </div>
   );
 }
